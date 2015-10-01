5da3ab2a1d13aa225c780135f5218e083d6921ac
sytyuk <yura.sytyuk@yandex.ua>
2015-10-01 11:21:55 +0300

    Constructor_Incapsulate

--- snake/snake/Point.cs
+++ snake/snake/Point.cs
@@ -12,6 +12,18 @@ namespace snake
         public int y;
         public char sym;
 
+        public Point()
+        {
+            
+        }
+
+        public Point(int _x, int _y, char _sym)
+        {
+            x = _x;
+            y = _y;
+            sym = _sym;
+        }
+
         public void Drow() {
             Console.SetCursorPosition(x, y);
             Console.Write(sym);
