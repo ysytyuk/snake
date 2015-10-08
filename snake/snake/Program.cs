b44d70b4057fb0a9d014e4e2957935f6cbf97331
Sytyuk <yura.sytyuk@yandex.ru>
2015-10-08 19:06:39 +0300

    list

--- snake/snake/Program.cs
+++ snake/snake/Program.cs
@@ -39,6 +39,8 @@ namespace snake
                 Console.WriteLine(i);
             }
 
+            numList.RemoveAt(0);
+
             List<Point> pList = new List<Point>();
             pList.Add(p1);
             pList.Add(p2);
