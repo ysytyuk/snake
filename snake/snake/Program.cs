5c9f94e058cdd2e327c28ce902e73383f227a195
Sytyuk <yura.sytyuk@yandex.ru>
2015-10-11 17:45:52 +0300

    master

--- snake/snake/Program.cs
+++ snake/snake/Program.cs
@@ -10,6 +10,7 @@ namespace snake
     {
         static void Main(string[] args)
         {
+
             Point p1 = new Point(1, 2, '*');
 
             p1.Drow();
@@ -18,40 +19,14 @@ namespace snake
 
             p2.Drow();
 
-            List<int> numList = new List<int>();
-            numList.Add(0);
-            numList.Add(1);
-            numList.Add(2);
-
-            List<char> charList = new List<char>();
-            charList.Add('$');
-            charList.Add('%');
-            charList.Add('@');
-            charList.Add('!');
-
-            foreach (char p in charList) {
-                Console.WriteLine(p);
-            }
-
-            foreach (int i in numList)
-            {
-                Console.WriteLine(i);
-            }
-
-            List<Point> pList = new List<Point>();
-            pList.Add(p1);
-            pList.Add(p2);
-
-            Point p3 = new Point(6, 7, '%');
-            Point p4 = new Point(7, 8, '$');
-            Point p5 = new Point(8, 9, '@');
-            Point p6 = new Point(9, 10, '&');
-
-            List<Point> myList = new List<Point>();
-            pList.Add(p3);
-            pList.Add(p4);
-            pList.Add(p5);
-            pList.Add(p6);
+            horizontalLine upLine = new horizontalLine(0, 78, 0, '+');
+            horizontalLine downLine = new horizontalLine(0, 78, 24, '+');
+            verticalLine leftline = new verticalLine(0, 24, 0, '+');
+            verticalLine rightline = new verticalLine(0, 24, 78, '+');
+            upLine.Draw();
+            downLine.Draw();
+            leftline.Draw();
+            rightline.Draw();
 
             Console.ReadLine();
         }
